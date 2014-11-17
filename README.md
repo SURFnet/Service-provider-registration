40aa33989b762cc1d0cb86c37dd8687f06d99539
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2014-11-17 12:07:00 +0100

    Added deployment script

--- README.md
+++ README.md
@@ -15,3 +15,28 @@ Requirements
 ### PHP
 * min. PHP 5.3.3
 * apc
+
+Deployment
+----------------------------------
+
+For now project is hosted on vps20.ibuildings.com
+
+### Capifony
+* cap deploy (uses ssh key forwarding for user root)
+
+### Manual
+* export SYMFONY_ENV=prod
+* git pull
+* php composer.phar composer install -o --no-dev
+* vi app/config.yml -> raise 'assets_version'
+* php app/console ass:dump
+
+Test Data
+----------------------------------
+
+Metadata URL's:
+
+* https://www.meertens.knaw.nl/Shibboleth.sso/Metadata
+* https://sts.hva.nl/federationmetadata/2007-06/federationmetadata.xml
+* https://info.acc.hsleiden.nl/OpenSAML.sso/Metadata
+* https://dmsonline.uvt.nl/simplesaml/module.php/saml/sp/metadata.php/surfconext-uvt 
---
+++ app/config/deploy.rb
@@ -0,0 +1,37 @@
+set :application, "SURFnet Service Provider Registration"
+set :domain,      "vps20.ibuildings.com"
+set :deploy_to,   "/var/www/surfform"
+set :app_path,    "app"
+
+set :repository, "git@github.com:SURFnet/Service-provider-registration.git"
+set :scm,        :git
+
+role :web, domain
+role :app, domain, :primary => true
+
+ssh_options[:forward_agent] = true
+set :user,                "root"
+set :use_sudo,            false
+
+set :permission_method,   :acl
+set :use_set_permissions, true
+set :writable_dirs,       ["app/cache", "app/logs", "app/data"]
+
+set :keep_releases, 3
+
+set :deploy_via, :remote_cache
+
+set :shared_files,    ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", app_path + "/data", web_path + "/uploads"]
+
+set :use_composer, true
+set :copy_vendors, true
+
+set :dump_assetic_assets,   true
+set :update_assets_version, true
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
+
+# Clean old releases after deploy
+after "deploy", "deploy:cleanup"
