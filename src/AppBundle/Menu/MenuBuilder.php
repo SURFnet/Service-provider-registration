5bde732a5a9c8588869b5c864c3663c9844fb3b0
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2014-11-18 16:44:48 +0100

    Added menu for backend

---
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class MenuBuilder
+{
+    /**
+     * @var FactoryInterface
+     */
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Knp\Menu\ItemInterface
+     */
+    public function createAdminMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Subscriptions', array('route' => 'admin.subscription.overview'));
+        $menu->addChild('Templates', array('route' => 'admin.template.overview'));
+        $menu->addChild('Texts', array('route' => 'admin.text.overview'));
+
+        return $menu;
+    }
+}
