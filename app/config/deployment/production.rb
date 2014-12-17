7d252709ff3e1c03545bc27ef10c9fd3dc1fb766
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2014-12-17 13:31:38 +0100

    Added custom task to production deployment script

--- app/config/deployment/production.rb
+++ app/config/deployment/production.rb
@@ -1,1 +1,11 @@
 server 'support.surfconext.nl', :app, :web, :primary => true
+
+before "deploy:create_symlink" do
+    custom.set_permissions
+end
+
+namespace :custom do
+    task :set_permissions, :roles => :app, :except => { :no_release => true } do
+        run "#{try_sudo} /usr/local/sbin/fix-www-permissions.sh /www/support/"
+    end
+end
