ed9c76f2df958dda481b54077fcd5b63b79ef8f4
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2015-04-17 09:11:44 +0200

    Store and show the active tab on page reloads. Fixes #39.

--- web/js/validate.js
+++ web/js/validate.js
@@ -221,6 +221,19 @@
             return false;
         });
 
+        // Show active tab on reload
+        if (location.hash.substr(0, 2) === '#!') {
+            $('a[href="#'  + location.hash.substr(2) + '"]').tab('show');
+        }
+
+        // Remember the hash in the URL without jumping
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+            var hash = $(e.target).attr('href');
+            if (hash.substr(0, 1) === '#') {
+                location.replace('#!' + hash.substr(1));
+            }
+        });
+
         // Setup help popovers
         $links.popover({
             container: 'body',
