7dd553e0c6cfea1d4eae753e07014cd96c39977a
Boy Baukema <boy@ibuildings.nl>
2015-09-21 09:58:52 +0200

    More work on syncing.

--- src/SURFnet/SPRegistration/Entity/ConnectionRequestTranslator.php
+++ src/SURFnet/SPRegistration/Entity/ConnectionRequestTranslator.php
@@ -2,9 +2,11 @@
 
 use AppBundle\Entity\Subscription;
 use AppBundle\Model\Attribute;
+use AppBundle\Model\Contact;
 use OpenConext\JanusClient\ArpAttributes;
 use OpenConext\JanusClient\ConnectionAccess;
 use OpenConext\JanusClient\Entity\Connection;
+use SURFnet\SPRegistration\ServiceRegistry\Consts as ServiceRegistry;
 
 class ConnectionRequestTranslator
 {
@@ -30,50 +32,73 @@ class ConnectionRequestTranslator
         Subscription $request
     )
     {
+        $request->setNameNl($connection->getMetadata(ServiceRegistry::NAME_NL));
+        $request->setNameEn($connection->getMetadata(ServiceRegistry::NAME_EN));
+        $request->setDescriptionEn($connection->getMetadata(ServiceRegistry::DESCRIPTION_EN));
+        $request->setDescriptionNl($connection->getMetadata(ServiceRegistry::DESCRIPTION_NL));
+        $request->setApplicationUrl($connection->getMetadata(ServiceRegistry::URL_EN));
+        $request->setEulaUrl($connection->getMetadata(ServiceRegistry::COIN_EULA));
+        $request->setLogoUrl($connection->getMetadata(ServiceRegistry::LOGO_0_URL));
+        $request->setAdministrativeContact(
+            $this->getContactOfType(ServiceRegistry::CONTACT_TYPE_ADMINISTRATIVE, $connection)
+        );
+        $request->setSupportContact(
+            $this->getContactOfType(ServiceRegistry::CONTACT_TYPE_SUPPORT, $connection)
+        );
+        $request->setTechnicalContact(
+            $this->getContactOfType(ServiceRegistry::CONTACT_TYPE_TECHNICAL, $connection)
+        );
+        $request->setAcsLocation(
+            $connection->getMetadata(ServiceRegistry::ASSERTIONCONSUMERSERVICE_0_LOCATION)
+        );
+        $certData = $connection->getMetadata('certData');
+        if ($certData) {
+            $request->setCertificate(SAML2_Certificate_X509::createFromCertificateData($certData)->getCertificate());
+        }
     }
 
     private function getMetadataFromRequest(Subscription $request)
     {
-        // @todo what happens on a 404.
+        // @todo what happens on a 404?
         list($width, $height) = getimagesize($request->getLogoUrl());
 
         return array(
-            'name:en'           => $request->getNameEn(),
-            'name:nl'           => $request->getNameNl(),
+            ServiceRegistry::NAME_EN                                => $request->getNameEn(),
+            ServiceRegistry::NAME_NL                                => $request->getNameNl(),
 
-            'description:en'    => $request->getDescriptionEn(),
-            'description:nl'    => $request->getDescriptionNl(),
+            ServiceRegistry::DESCRIPTION_EN                         => $request->getDescriptionEn(),
+            ServiceRegistry::DESCRIPTION_NL                         => $request->getDescriptionNl(),
+            ServiceRegistry::URL_NL                                 => $request->getApplicationUrl(),
+            ServiceRegistry::URL_EN                                 => $request->getApplicationUrl(),
+            ServiceRegistry::COIN_EULA                              => $request->getEulaUrl(),
 
-            'url:nl'            => $request->getApplicationUrl(),
-            'url:en'            => $request->getApplicationUrl(),
+            ServiceRegistry::NAMEIDFORMAT                           => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
 
-            'NameIDFormat'      => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+            ServiceRegistry::LOGO_0_URL                             => $request->getLogoUrl(),
+            ServiceRegistry::LOGO_0_WIDTH                           => $width,
+            ServiceRegistry::LOGO_0_HEIGHT                          => $height,
 
-            'logo:0:url'                    => $request->getLogoUrl(),
-            'logo:0:width'                  => $width,
-            'logo:0:height'                 => $height,
+            ServiceRegistry::CONTACTS_0_CONTACTTYPE                 => ServiceRegistry::CONTACT_TYPE_TECHNICAL,
+            ServiceRegistry::CONTACTS_0_EMAILADDRESS                => $request->getTechnicalContact()->getEmail(),
+            ServiceRegistry::CONTACTS_0_GIVENNAME                   => $request->getTechnicalContact()->getFirstName(),
+            ServiceRegistry::CONTACTS_0_SURNAME                     => $request->getTechnicalContact()->getLastName(),
+            ServiceRegistry::CONTACTS_0_TELEPHONENUMBER             => $request->getTechnicalContact()->getPhone(),
 
-            'contacts:0:contactType'        => 'technical',
-            'contacts:0:emailAddress'       => $request->getTechnicalContact()->getEmail(),
-            'contacts:0:givenName'          => $request->getTechnicalContact()->getFirstName(),
-            'contacts:0:surName'            => $request->getTechnicalContact()->getLastName(),
-            'contacts:0:telephoneNumber'    => $request->getTechnicalContact()->getPhone(),
+            ServiceRegistry::CONTACTS_1_CONTACTTYPE                 => ServiceRegistry::CONTACT_TYPE_SUPPORT,
+            ServiceRegistry::CONTACTS_1_EMAILADDRESS                => $request->getSupportContact()->getEmail(),
+            ServiceRegistry::CONTACTS_1_GIVENNAME                   => $request->getSupportContact()->getFirstName(),
+            ServiceRegistry::CONTACTS_1_SURNAME                     => $request->getSupportContact()->getLastName(),
+            ServiceRegistry::CONTACTS_1_TELEPHONENUMBER             => $request->getSupportContact()->getPhone(),
 
-            'contacts:1:contactType'        => 'support',
-            'contacts:1:emailAddress'       => $request->getSupportContact()->getEmail(),
-            'contacts:1:givenName'          => $request->getSupportContact()->getFirstName(),
-            'contacts:1:surName'            => $request->getSupportContact()->getLastName(),
-            'contacts:1:telephoneNumber'    => $request->getSupportContact()->getPhone(),
+            ServiceRegistry::CONTACTS_2_CONTACTTYPE                 => ServiceRegistry::CONTACT_TYPE_ADMINISTRATIVE,
+            ServiceRegistry::CONTACTS_2_EMAILADDRESS                => $request->getAdministrativeContact()->getEmail(),
+            ServiceRegistry::CONTACTS_2_GIVENNAME                   => $request->getAdministrativeContact()->getFirstName(),
+            ServiceRegistry::CONTACTS_2_SURNAME                     => $request->getAdministrativeContact()->getLastName(),
+            ServiceRegistry::CONTACTS_2_TELEPHONENUMBER             => $request->getAdministrativeContact()->getPhone(),
 
-            'contacts:2:contactType'        => 'administrative',
-            'contacts:2:emailAddress'       => $request->getAdministrativeContact()->getEmail(),
-            'contacts:2:givenName'          => $request->getAdministrativeContact()->getFirstName(),
-            'contacts:2:surName'            => $request->getAdministrativeContact()->getLastName(),
-            'contacts:2:telephoneNumber'    => $request->getAdministrativeContact()->getPhone(),
-
-            'AssertionConsumerService:0:Binding'  => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-            'AssertionConsumerService:0:Location' => $request->getAcsLocation(),
-            'certData' => $this->mapX509ToCertData($request->getCertificate()),
+            ServiceRegistry::ASSERTIONCONSUMERSERVICE_0_BINDING     => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+            ServiceRegistry::ASSERTIONCONSUMERSERVICE_0_LOCATION    => $request->getAcsLocation(),
+            ServiceRegistry::CERTDATA                               => $this->mapX509ToCertData($request->getCertificate()),
         );
     }
 
@@ -229,5 +254,85 @@ class ConnectionRequestTranslator
                 'friendlyName' => 'Employee/student number'
             )
         );
+    }
+
+    private function getContactOfType($contactType, Connection $connection)
+    {
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_0_CONTACTTYPE)) {
+            if ($connection->getMetadata(ServiceRegistry::CONTACTS_0_CONTACTTYPE) === $contactType) {
+                return $this->getContact0($connection);
+            }
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_1_CONTACTTYPE)) {
+            if ($connection->getMetadata(ServiceRegistry::CONTACTS_1_CONTACTTYPE) === $contactType) {
+                return $this->getContact1($connection);
+            }
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_2_CONTACTTYPE)) {
+            if ($connection->getMetadata(ServiceRegistry::CONTACTS_2_CONTACTTYPE) === $contactType) {
+                return $this->getContact2($connection);
+            }
+        }
+        return NULL;
+    }
+
+    private function getContact0(Connection $connection)
+    {
+        $contact = new Contact();
+
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_0_GIVENNAME)) {
+            $contact->setFirstName($connection->getMetadata(ServiceRegistry::CONTACTS_0_GIVENNAME));
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_0_SURNAME)) {
+            $contact->setLastName($connection->getMetadata(ServiceRegistry::CONTACTS_0_SURNAME));
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_0_EMAILADDRESS)) {
+            $contact->setEmail($connection->getMetadata(ServiceRegistry::CONTACTS_0_EMAILADDRESS));
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_0_TELEPHONENUMBER)) {
+            $contact->setPhone($connection->getMetadata(ServiceRegistry::CONTACTS_0_TELEPHONENUMBER));
+        }
+
+        return $contact;
+    }
+
+    private function getContact1(Connection $connection)
+    {
+        $contact = new Contact();
+
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_1_GIVENNAME)) {
+            $contact->setFirstName($connection->getMetadata(ServiceRegistry::CONTACTS_1_GIVENNAME));
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_1_SURNAME)) {
+            $contact->setLastName($connection->getMetadata(ServiceRegistry::CONTACTS_1_SURNAME));
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_1_EMAILADDRESS)) {
+            $contact->setEmail($connection->getMetadata(ServiceRegistry::CONTACTS_1_EMAILADDRESS));
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_1_TELEPHONENUMBER)) {
+            $contact->setPhone($connection->getMetadata(ServiceRegistry::CONTACTS_1_TELEPHONENUMBER));
+        }
+
+        return $contact;
+    }
+
+    private function getContact2(Connection $connection)
+    {
+        $contact = new Contact();
+
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_2_GIVENNAME)) {
+            $contact->setFirstName($connection->getMetadata(ServiceRegistry::CONTACTS_2_GIVENNAME));
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_2_SURNAME)) {
+            $contact->setLastName($connection->getMetadata(ServiceRegistry::CONTACTS_2_SURNAME));
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_2_EMAILADDRESS)) {
+            $contact->setEmail($connection->getMetadata(ServiceRegistry::CONTACTS_2_EMAILADDRESS));
+        }
+        if ($connection->hasMetadata(ServiceRegistry::CONTACTS_2_TELEPHONENUMBER)) {
+            $contact->setPhone($connection->getMetadata(ServiceRegistry::CONTACTS_2_TELEPHONENUMBER));
+        }
+
+        return $contact;
     }
 }
---
+++ src/SURFnet/SPRegistration/ServiceRegistry/Consts.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SURFnet\SPRegistration\ServiceRegistry;
+
+class Consts
+{
+    const NAME_EN                               = 'name:en';
+    const NAME_NL                               = 'name:nl';
+
+    const DESCRIPTION_EN                        = 'description:en';
+    const DESCRIPTION_NL                        = 'description:nl';
+
+    const URL_NL                                = 'url:nl';
+    const URL_EN                                = 'url:en';
+
+    const NAMEIDFORMAT                          = 'NameIDFormat';
+
+    const LOGO_0_URL                            = 'logo:0:url';
+    const LOGO_0_WIDTH                          = 'logo:0:width';
+    const LOGO_0_HEIGHT                         = 'logo:0:height';
+
+    const CONTACT_TYPE_TECHNICAL                = 'technical';
+    const CONTACT_TYPE_SUPPORT                  = 'support';
+    const CONTACT_TYPE_ADMINISTRATIVE           = 'administrative';
+
+    const CONTACTS_0_CONTACTTYPE                = 'contacts:0:contactType';
+    const CONTACTS_0_EMAILADDRESS               = 'contacts:0:emailAddress';
+    const CONTACTS_0_GIVENNAME                  = 'contacts:0:givenName';
+    const CONTACTS_0_SURNAME                    = 'contacts:0:surName';
+    const CONTACTS_0_TELEPHONENUMBER            = 'contacts:0:telephoneNumber';
+
+    const CONTACTS_1_CONTACTTYPE                = 'contacts:1:contactType';
+    const CONTACTS_1_EMAILADDRESS               = 'contacts:1:emailAddress';
+    const CONTACTS_1_GIVENNAME                  = 'contacts:1:givenName';
+    const CONTACTS_1_SURNAME                    = 'contacts:1:surName';
+    const CONTACTS_1_TELEPHONENUMBER            = 'contacts:1:telephoneNumber';
+
+    const CONTACTS_2_CONTACTTYPE                = 'contacts:2:contactType';
+    const CONTACTS_2_EMAILADDRESS               = 'contacts:2:emailAddress';
+    const CONTACTS_2_GIVENNAME                  = 'contacts:2:givenName';
+    const CONTACTS_2_SURNAME                    = 'contacts:2:surName';
+    const CONTACTS_2_TELEPHONENUMBER            = 'contacts:2:telephoneNumber';
+
+    const ASSERTIONCONSUMERSERVICE_0_BINDING    = 'AssertionConsumerService:0:Binding';
+    const ASSERTIONCONSUMERSERVICE_0_LOCATION   = 'AssertionConsumerService:0:Location';
+    const CERTDATA                              = 'certData';
+
+    const COIN_EULA                             = 'coin:eula';
+}
