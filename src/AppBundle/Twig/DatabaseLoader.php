54e8ecedfa84b8edbd71b1a6a28f13a89c61473a
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2015-09-23 08:28:52 +0200

    Fixed issue with long tpl-names.

--- src/AppBundle/Twig/DatabaseLoader.php
+++ src/AppBundle/Twig/DatabaseLoader.php
@@ -90,13 +90,14 @@ class DatabaseLoader implements Twig_LoaderInterface, Twig_ExistsLoaderInterface
      */
     protected function getTemplate($name, $force = false)
     {
-        if (!$force && false !== $tpl = $this->cache->fetch('tpl-' . $name)) {
+        $cacheId = md5('tpl-' . $name);
+        if (!$force && false !== $tpl = $this->cache->fetch($cacheId)) {
             return $tpl;
         }
 
         $tpl = $this->em->getRepository('AppBundle:Template')->findOneBy(array('name' => $name));
 
-        $this->cache->save('tpl-' . $name, $tpl, 60 * 60 * 24);
+        $this->cache->save($cacheId, $tpl, 60 * 60 * 24);
 
         return $tpl;
     }
