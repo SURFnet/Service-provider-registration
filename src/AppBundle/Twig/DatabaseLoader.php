0b5ecfdfed9e90250a22b7fdfb0258da82813b39
Boy Baukema <boy@ibuildings.nl>
2015-09-24 11:25:04 +0200

    Refactor DatabaseLoader->getTemplate for increased readability and robustness (if NULL is inadvertedly cached)

--- src/AppBundle/Twig/DatabaseLoader.php
+++ src/AppBundle/Twig/DatabaseLoader.php
@@ -91,8 +91,12 @@ class DatabaseLoader implements Twig_LoaderInterface, Twig_ExistsLoaderInterface
     protected function getTemplate($name, $force = false)
     {
         $cacheId = md5('tpl-' . $name);
-        if (!$force && false !== $tpl = $this->cache->fetch($cacheId)) {
-            return $tpl;
+
+        if (!$force) {
+            $tpl = $this->cache->fetch($cacheId);
+            if ($tpl) {
+                return $tpl;
+            }
         }
 
         $tpl = $this->em->getRepository('AppBundle:Template')->findOneBy(array('name' => $name));
