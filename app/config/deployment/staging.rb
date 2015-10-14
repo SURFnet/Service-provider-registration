5b6cfdc3a423ae7bd318d7e78022663b8be0ed25
Bas Zoetekouw <bas@zoetekouw.net>
2015-10-14 16:53:15 +0200

    Fix deployment to staging

--- app/config/deployment/staging.rb
+++ app/config/deployment/staging.rb
@@ -1,3 +1,16 @@
-server 'server.domain.tld', :app, :web, :primary => true
+server 'info2.surfnet.nl', :app, :web, :primary => true
+#set :ssh_options, { verbose: :debug }
+
+set :deploy_to,   "/www/support/spform-staging/"
 
 default_run_options[:pty] = true
+
+before "deploy:create_symlink" do
+	custom.set_permissions
+end
+
+namespace :custom do
+	task :set_permissions, :roles => :app, :except => { :no_release => true } do
+		sudo "/usr/local/sbin/fix-www-permissions.sh /www/support/"
+	end
+end
