28e459bbe609a3ee63d4858b63a1fd3581fbd4d1
Boy Baukema <boy@ibuildings.nl>
2015-11-02 11:58:18 +0100

    Fixes #56 Memcache flush on deploy
    https://www.pivotaltracker.com/n/projects/1400064/stories/106834750

--- app/config/deployment/deploy.rb
+++ app/config/deployment/deploy.rb
@@ -56,6 +56,31 @@ namespace :symfony do
   end
 end
 
+require 'socket'
+
+namespace :memcached do
+  desc 'Flushes whole memcached local instance'
+  task :flush do
+    server  = '127.0.0.1'
+    port    = 11211
+    command = "flush_all\r\n"
+
+    socket = TCPSocket.new(server, port)
+    socket.write(command)
+    result = socket.recv(2)
+
+    if result != 'OK'
+      STDERR.puts "Error flushing memcached: #{result}"
+    end
+
+    socket.close
+  end
+end
+
+after 'deploy:update' do
+  memcached.flush
+end
+
 after "symfony:update_translations", "symfony:update_templates"
 
 # Clean old releases after deploy
