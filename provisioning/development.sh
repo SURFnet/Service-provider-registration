398a6443ad0944b4c1c7366b554ccea0bbff3cdd
Boy Baukema <boy@ibuildings.nl>
2016-07-12 10:56:35 +0200

    Turn on provisioning of other components

--- provisioning/development.sh
+++ provisioning/development.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "127.0.0.1     serviceregistry.dev.surfconext.nl" >> /etc/hosts
 
-#yum -y -q install vim
-#
-#source /vagrant/provisioning/scripts/httpd.sh
-#source /vagrant/provisioning/scripts/php.sh
+yum -y -q install vim
+
+source /vagrant/provisioning/scripts/httpd.sh
+source /vagrant/provisioning/scripts/php.sh
 
 source /vagrant/provisioning/serviceregistry/serviceregistry.sh
 
-#source /vagrant/provisioning/app/app-dev.sh
-#source /vagrant/provisioning/app/app.sh
+source /vagrant/provisioning/app/app-dev.sh
+source /vagrant/provisioning/app/app.sh
