1d595407865515bb34151fb772d97bd184cee203
Boy Baukema <boy@ibuildings.nl>
2016-07-14 11:30:04 +0200

    Provision XDebug too

--- provisioning/scripts/php.sh
+++ provisioning/scripts/php.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-yum -y -q install php-apc php-gd php-mbstring php-mcrypt
+yum -y -q install php-apc php-gd php-mbstring php-mcrypt php-pecl-xdebug
 
 yum -y -q remove php-pecl-memcached
 yum -y -q install php-pecl-memcache
 
 chmod -R 777 /var/lib/php/session
+
+cp /vagrant/provisioning/scripts/xdebug.ini /etc/php.d/xdebug.ini
